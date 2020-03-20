551c0774a82a44a83ac9f0481e8de2198634d3cc
kazushi <kimoto@okayama-u.ac.jp>
2020-03-21 00:39:54 +0900

    index retained in the heap

--- grain.cpp
+++ grain.cpp
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <climits>
 
 class Grain{
 	public:
 		int **kcell;
-		double Xa[2],Xb[2],Xc[2];
+		double Xa[2],Xb[2],Xc[2],dx[2];
 		double Wd[2];
-		double Ndiv[2];
+		int Ndiv[2];
 		int ncell;
 		void init(int nx, int ny);
 		void print();
+		void fprint(char fn[128]);
 		void set_size(double wx, double wy);
 		void set_pos(double x0, double y0);
+		void star();
+		int *far;
+		int *known;
+		int *trial;
+		int nfr,nkw,ntr;
 	private:
 };
 
@@ -22,8 +30,17 @@ void Grain::init(int nx, int ny){
 	int *pt=(int *)malloc(sizeof(int)*ncell);
 	kcell=(int **)malloc(sizeof(int*)*nx);
 
+	far=(int *)malloc(sizeof(int)*ncell);
+	trial=(int *)malloc(sizeof(int)*ncell);
+	known=(int *)malloc(sizeof(int)*ncell);
+
 	int i;
-	for(i=0;i<ncell;i++) pt[i]=i;
+	for(i=0;i<ncell;i++){
+		pt[i]=0;
+		far[i]=-1;
+		trial[i]=-1;
+		known[i]=-1;
+	};
 	for(i=0;i<nx;i++) kcell[i]=pt+i*ny;
 };
 void Grain::print(){
@@ -35,21 +52,105 @@ void Grain::print(){
 	printf("\n");
 	}
 };
+void Grain::fprint(char fn[128]){
+	FILE *fp=fopen(fn,"w");
+	int i,j;
+	fprintf(fp,"# Xa[0:2],Xb[0:2]\n");
+	fprintf(fp,"%lf, %lf\n",Xa[0],Xa[1]);
+	fprintf(fp,"%lf, %lf\n",Xb[0],Xb[1]);
+	fprintf(fp,"# Ndat[0:2]\n");
+	fprintf(fp,"%d, %d\n",Ndiv[0],Ndiv[1]);
+	fprintf(fp,"# val\n");
+	for(i=0;i<Ndiv[0];i++){
+	for(j=0;j<Ndiv[1];j++){
+		fprintf(fp,"%d\n",kcell[i][j]);
+	}
+	}
+	fclose(fp);
+};
 void Grain::set_size(double wx, double wy){
 	Wd[0]=wx;
 	Wd[1]=wy;
+	wx*=0.5;
+	wy*=0.5;
+	Xa[0]=Xc[0]-wx;
+	Xa[1]=Xc[1]-wy;
+	Xb[0]=Xc[0]+wx;
+	Xb[1]=Xc[1]+wy;
+	dx[0]=Wd[0]/Ndiv[0];
+	dx[1]=Wd[1]/Ndiv[1];
 };
 void Grain::set_pos(double x0, double y0){
 	Xc[0]=x0;
 	Xc[1]=y0;
 };
+void Grain::star(){
+	double pi=4.0*atan(1.0);
+	int Nth=180;
+	double rr,th,dth=2.*pi/Nth;
+	int i,j,ix,iy;
+	double r0=0.2,r1=0.6;
+	double xx,yy;
+	for(i=0;i<Nth;i++){
+		th=dth*i;
+		rr=(r1-r0)*cos(2.*th)*cos(2.*th)+r0;
+		xx=Xc[0]+rr*cos(th);
+		yy=Xc[0]+rr*sin(th);
+		ix=int((xx-Xa[0])/dx[0]);
+		iy=int((yy-Xa[1])/dx[1]);
+		if(ix<0) continue;
+		if(ix>=Ndiv[0]) continue;
+		if(iy<0) continue;
+		if(iy>=Ndiv[1]) continue;
+		kcell[ix][iy]=1;
+	}
+
+	int I,J;
+	for(i=0;i<Ndiv[0];i++){
+	for(j=0;j<Ndiv[1];j++){
+		//if(kcell[i][j]==0) kcell[i][j]=INT_MAX;
+		if(kcell[i][j]==1){
+			I=i+1;
+			J=j+1;
+			if(I>=Ndiv[0]) I=i;
+			if(J>=Ndiv[1]) J=j;
+			if(kcell[I][j]!=1)kcell[I][j]=2;
+			if(kcell[i][J]!=1) kcell[i][J]=2;
+			I=i-1;
+			J=j-1;
+			if(I<0) I=i;
+			if(J<0) J=j;
+			if(kcell[I][j]!=1) kcell[I][j]=2;
+			if(kcell[i][J]!=1) kcell[i][J]=2;
+
+		}
+	}
+	}
+
+	nkw=0;
+	ntr=0;
+	nfr=0;
+	for(i=0;i<Ndiv[0];i++){
+	for(j=0;j<Ndiv[1];j++){
+		I=i*Ndiv[1]+j;
+		if(kcell[i][j]==0) far[nfr++]=I;
+		if(kcell[i][j]==1) known[nkw++]=I;
+		if(kcell[i][j]==2) trial[ntr++]=I;
+
+	}
+	}
+	printf("nkw=%d, ntr=%d, nfr=%d (n=%d)\n",nkw,ntr,nfr,nkw+ntr+nfr);
+};
 int main(){
 	Grain gr;
 
-	gr.init(20,20);
+	gr.init(40,40);
 	gr.set_size(2.0,2.0);
 	gr.set_pos(0.0,0.0);
+	gr.star();
 	gr.print();
+	char fname[128]="kcell.dat";
+	gr.fprint(fname);
 
 	return(0);
 };
--- heap_stash.cpp
+++ heap_stash.cpp
@@ -6,14 +6,13 @@ class Heap{
 	public:
 		double *dat;	// data 
 		double *hp;	// heap 
-		int *indx;	// index
 		int nsize;	// array size
 		int ndat;	// number of data
 		void init(int n); 
 		void print();
 		void heap();
 		void sort();
-		double pop(int *i0);
+		double pop();
 		void  add2heap(double val);
 	private:
 
@@ -24,17 +23,14 @@ void Heap::init(int n){
 	ndat=0;
 	dat=(double *)malloc(sizeof(double)*nsize);
 	hp=(double *)malloc(sizeof(double)*nsize);
-	indx=(int *)malloc(sizeof(int)*nsize);
 	for(int i=0;i<nsize;i++){
 		dat[i]=0.0;
 		hp[i]=0.0;
-		indx[i]=i;
 	}
 };
 void Heap::heap(){
 	int i,c,p;
 	double tmp;
-	int itmp;
 	for(i=0;i<ndat;i++){
 		hp[i]=dat[i];
 		c=i+1;
@@ -43,10 +39,6 @@ void Heap::heap(){
 			tmp=hp[p-1];
 			hp[p-1]=hp[c-1];
 			hp[c-1]=tmp;
-
-			itmp=indx[p-1];
-			indx[p-1]=indx[c-1];
-			indx[c-1]=itmp;
 			c=p;
 			p=c/2;
 			if(p==0) break;
@@ -65,34 +57,26 @@ void Heap::add2heap(double val){
 	};
 	dat[ndat-1]=val;
 	hp[ndat-1]=val;
-	indx[ndat-1]=ndat-1;
 	double tmp;
 	int c=ndat;
 	int p=ndat/2;
-	int itmp;
 	while(hp[p-1]>hp[c-1]){
 		tmp=hp[p-1];
 		hp[p-1]=hp[c-1];
 		hp[c-1]=tmp;
-
-		itmp=indx[p-1];
-		indx[p-1]=indx[c-1];
-		indx[c-1]=itmp;
 		c=p;
 		p=c/2;
 		if(p==0) break;
 	};
 };
 
-double Heap::pop(int *i0){
+double Heap::pop(){
 	double tmp,dat0=hp[0];
 	hp[0]=hp[ndat-1];
 	ndat--;
-	*i0=indx[0];
 
 	int c,c1,c2,p=1;
 	c1=p*2;
-	int itmp;
 	while(c1<=ndat){
 		c2=c1+1;
 		c=c1;
@@ -103,31 +87,21 @@ double Heap::pop(int *i0){
 		tmp=hp[p-1];
 		hp[p-1]=hp[c-1];
 		hp[c-1]=tmp;
-
-		itmp=indx[p-1];
-		indx[p-1]=indx[c-1];
-		indx[c-1]=itmp;
 		p=c;
 		c1=p*2;
 	};
 	return(dat0);
 };
-
 void Heap::sort(){
 	int i,p,c,c1,c2;
 	int n=ndat;
 	double tmp;
-	int itmp;
 
 	Heap::heap();
 	for(i=0;i<ndat;i++){
 		tmp=hp[0];
 		hp[0]=hp[n-1];
 		hp[n-1]=tmp;
-
-		itmp=indx[0];
-		indx[0]=indx[n-1];
-		indx[n-1]=itmp;
 		n--;
 
 		p=1;
@@ -142,10 +116,6 @@ void Heap::sort(){
 			tmp=hp[p-1];
 			hp[p-1]=hp[c-1];
 			hp[c-1]=tmp;
-
-			itmp=indx[p-1];
-			indx[p-1]=indx[c-1];
-			indx[c-1]=itmp;
 			p=c;
 			c1=p*2;
 		}
@@ -153,7 +123,7 @@ void Heap::sort(){
 };
 void Heap::print(){
 	//for(int i=0;i<nsize;i++) printf("%d %lf %lf\n",i,dat[i],hp[i]);
-	for(int i=0;i<ndat;i++) printf("%d %lf %d %lf\n",i,dat[i],indx[i],hp[i]);
+	for(int i=0;i<ndat;i++) printf("%d %lf %lf\n",i,dat[i],hp[i]);
 	puts("----------------------------");
 };
 int main(){
@@ -161,6 +131,7 @@ int main(){
 	int i,nsize=22,ndat=21;
 	hp.init(nsize);
 	hp.ndat=ndat;
+
 	for(i=0;i<ndat;i++){
 		hp.dat[i]=sin(double(i));
 	};
@@ -169,22 +140,10 @@ int main(){
 	hp.add2heap(0.3);
 	hp.print();
 
-	int i0;
-	double dat0;
-	for(i=0;i<ndat;i++){
-		dat0=hp.pop(&i0);
-	       	printf("%lf %lf\n",dat0,hp.dat[i0]);
-	}
+	for(i=0;i<ndat;i++) printf("%lf\n",hp.pop());
+	exit(-1);
 	puts("-------------------");
-
-
-	hp.ndat=ndat;
-	for(i=0;i<ndat;i++){
-		hp.dat[i]=sin(double(i));
-	};
-
 	hp.heap();
-	hp.add2heap(0.3);
 	hp.print();
 	puts("-------------------");
 	hp.sort();
